29fb47e8303f837d5ef00b3173488bf7d770ac5d
vatul16 <vishwa.atul16@gmail.com>
2025-12-02 16:07:24 +0530

    constatnts local package

---
+++ packages/constants/dist/index.d.ts
@@ -0,0 +1,9 @@
+export declare const USER_TOPICS: {
+    ACCOUNT_CREATED: string;
+    ACCOUNT_DELETED: string;
+    USER_REGISTERED: string;
+};
+export declare const ERROR_CODES: {
+    ACCOUNT_NOT_FOUND: string;
+    INSUFFICIENT_BALANCE: string;
+};
---
+++ packages/constants/dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ERROR_CODES = exports.USER_TOPICS = void 0;
+exports.USER_TOPICS = {
+    ACCOUNT_CREATED: "account.created",
+    ACCOUNT_DELETED: "account.deleted",
+    USER_REGISTERED: "user.registered",
+};
+exports.ERROR_CODES = {
+    ACCOUNT_NOT_FOUND: "ET01",
+    INSUFFICIENT_BALANCE: "ET02",
+};
